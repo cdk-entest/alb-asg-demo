21fe0c012ca8c12285fbd2cefd3f1a1707470e17
entest <hai@entest.io>
2022-08-09 02:42:52 +0000

    add package.json

--- web/app.py
+++ web/app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+import socket
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path="", static_folder="static")
+app = Flask(
+    __name__, static_url_path="", static_folder="static", template_folder="template"
+)
 
 
 @app.route("/")
 def hello_world():
     return app.send_static_file("index.html")
+
+
+@app.route("/host")
+def get_host():
+    host = socket.gethostname()
+    return render_template("host.html", host=host)
 
 
 if __name__ == "__main__":
