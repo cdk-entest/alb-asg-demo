01207f1959f2dd5fe3e055d8ee9c76c975331a22
entest <hai@entst.io>
2023-04-18 09:23:20 +0000

    test

--- bin/alb-asg-demo.ts
+++ bin/alb-asg-demo.ts
@@ -3,24 +3,46 @@ import * as cdk from "aws-cdk-lib";
 import {
   VpcStack,
   ApplicationStack,
+  ImportedVpcStack,
 } from "../lib/alb-asg-demo-stack";
+
+const mode = "existedVpc";
 
 const app = new cdk.App();
 
-// vpc stack
-const vpc = new VpcStack(app, "VpcStack", {
-  cidr: "10.1.0.0/20",
-  env: {
-    region: "us-east-1",
-  },
-});
+// ========================= look up existed vpc
+if (mode == "existedVpc") {
+  const network = new ImportedVpcStack(app, "LookupExistedVpc", {
+    vpcName: "VpcForRdsEc2",
+    vpcId: "vpc-049d70b38566687a6",
+    env: {
+      region: "us-east-1",
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+    },
+  });
 
-// application stack
-const alb = new ApplicationStack(app, "ApplicationStack", {
-  vpc: vpc.vpc,
-  env: {
-    region: "us-east-1",
-  },
-});
+  new ApplicationStack(app, "ApplicationStack", {
+    vpc: network.vpc,
+    env: {
+      region: "us-east-1",
+    },
+  });
+} else {
+  // ========================= new vpc stack
+  const network = new VpcStack(app, "VpcStack", {
+    cidr: "192.168.0.0/16",
+    env: {
+      region: "us-east-1",
+    },
+  });
 
-alb.addDependency(vpc);
+  // application stack
+  const alb = new ApplicationStack(app, "ApplicationStack", {
+    vpc: network.vpc,
+    env: {
+      region: "us-east-1",
+    },
+  });
+
+  alb.addDependency(network);
+}
