2a40c5cd95aac2ea4d643718f676510ea16ec2f6
entest <hai@entst.io>
2023-04-28 03:49:27 +0000

    ok

--- bin/alb-asg-demo.ts
+++ bin/alb-asg-demo.ts
@@ -4,23 +4,41 @@ import {
   VpcStack,
   ApplicationStack,
 } from "../lib/alb-asg-demo-stack";
+import {
+  NetworkStack,
+  WebServerStack,
+} from "../test/hello";
 
 const app = new cdk.App();
 
-// vpc stack
-const vpc = new VpcStack(app, "VpcStack", {
-  cidr: "10.1.0.0/20",
+// // vpc stack
+// const vpc = new VpcStack(app, "VpcStack", {
+//   cidr: "10.1.0.0/20",
+//   env: {
+//     region: "us-east-1",
+//   },
+// });
+
+// // application stack
+// const alb = new ApplicationStack(app, "ApplicationStack", {
+//   vpc: vpc.vpc,
+//   env: {
+//     region: "us-east-1",
+//   },
+// });
+
+// alb.addDependency(vpc);
+
+//
+const net = new NetworkStack(app, "NetworkStack", {
   env: {
     region: "us-east-1",
   },
 });
 
-// application stack
-const alb = new ApplicationStack(app, "ApplicationStack", {
-  vpc: vpc.vpc,
+const web = new WebServerStack(app, "WebServerStack", {
+  vpc: net.vpc,
   env: {
     region: "us-east-1",
   },
 });
-
-alb.addDependency(vpc);
